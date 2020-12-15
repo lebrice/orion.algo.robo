da12a4665e6ffec7afd2c656d9f2b85c66aec119
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-12-15 16:01:05 -0500

    Add Jinja2 dep

--- scripts/setup_gh.sh
+++ scripts/setup_gh.sh
@@ -2,5 +2,5 @@ python -m pip install --upgrade pip
 pip install tox tox-gh-actions
 sudo add-apt-repository ppa:teward/swig3.0 -y
 sudo apt-get install -y libeigen3-dev swig3.0
-pip install PyYAML
+pip install PyYAML Jinja2
 pip install -r requirements.txt
