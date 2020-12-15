cfa42869ed10b08001eadfa88a1b05bc49d4e85a
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-12-15 15:53:10 -0500

    PyYAML...

--- scripts/setup_gh.sh
+++ scripts/setup_gh.sh
@@ -2,5 +2,5 @@ python -m pip install --upgrade pip
 pip install tox tox-gh-actions
 sudo add-apt-repository ppa:teward/swig3.0 -y
 sudo apt-get install -y libeigen3-dev swig3.0
-pip install yaml
+pip install PyYAML
 pip install -r requirements.txt
