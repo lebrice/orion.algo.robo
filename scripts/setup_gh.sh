f16323e0818e5c2e8fffe372da347dfb7a1a5e8f
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-12-16 10:02:15 -0500

    List installed packages to see if george was previously installed

--- scripts/setup_gh.sh
+++ scripts/setup_gh.sh
@@ -3,4 +3,6 @@ pip install tox tox-gh-actions
 sudo add-apt-repository ppa:teward/swig3.0 -y
 sudo apt-get install -y libeigen3-dev swig3.0
 pip install PyYAML Jinja2
+pip list
 pip install -r requirements.txt
+pip install -e .
