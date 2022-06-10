f976f2dfa9a0cdd796c76bedec4815b2000214b0
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-06-10 14:35:28 -0400

    Clean up type annotations on Normal class
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- src/orion/algo/robo/ablr/normal.py
+++ src/orion/algo/robo/ablr/normal.py
@@ -1,38 +1,47 @@
 """Patch for `torch.distributions.Normal`, so that we can add/multiply distributions by constants.
 """
+from __future__ import annotations
 
-from typing import Any, Optional, Union
+from typing import Any, TypeVar
 
 from torch import Tensor
 from torch.distributions import Normal as NormalBase
 
+C = TypeVar("C", int, float, Tensor)
+
 
 class Normal(NormalBase):
-    def __init__(
-        self, loc: Tensor, scale: Tensor, validate_args: Optional[bool] = None
-    ):
+    def __init__(self, loc: Tensor, scale: Tensor, validate_args: bool | None = None):
         super().__init__(loc=loc, scale=scale, validate_args=validate_args)
 
-    def __add__(self, other: Union["Normal", Any]) -> "Normal":
+    def __add__(self, other: C) -> Normal:
         if isinstance(other, (int, float, Tensor)):
             return Normal(self.mean + other, scale=self.scale)
         if isinstance(other, NormalBase):
             raise NotImplementedError("Only support addition with a constant for now")
         return NotImplemented
 
-    def __radd__(self, other: Union["Normal", Any]) -> "Normal":
+    def __iadd__(self, other: C) -> Normal:
+        if isinstance(other, (int, float, Tensor)):
+            self.loc += other
+            return self
+        if isinstance(other, NormalBase):
+            raise NotImplementedError("Only support addition with a constant for now")
+        return NotImplemented
+
+    def __radd__(self, other: C | Any) -> Normal:
         return self + other
 
-    def __sub__(self, other: Union["Normal", Any]) -> "Normal":
+    def __sub__(self, other: C) -> Normal:
         return self + (-other)
 
-    def __rsub__(self, other: Union["Normal", Any]) -> "Normal":
+    def __rsub__(self, other: C | Any) -> Normal:
         return (-self) + other
 
-    def __neg__(self) -> "Normal":
+    def __neg__(self) -> Normal:
         return (-1) * self
 
-    def __mul__(self, other: Union["Normal", Any]) -> "Normal":
+    def __mul__(self, other: C) -> Normal:
         if isinstance(other, (int, float, Tensor)):
             # TODO: Double-check that this is correct.
             return Normal(self.mean * other, scale=self.scale * other**2)
@@ -40,13 +49,21 @@ class Normal(NormalBase):
             raise NotImplementedError("Only support multiplication by constant for now")
         return NotImplemented
 
-    def __rmul__(self, other: Union["Normal", Any]) -> "Normal":
+    def __imul__(self, other: C) -> Normal:
+        if isinstance(other, (int, float, Tensor)):
+            self.loc *= other
+            self.scale *= other**2
+            return self
+        if isinstance(other, NormalBase):
+            raise NotImplementedError("Only support multiplication by constant for now")
+        return NotImplemented
+
+    def __rmul__(self, other: C | Any) -> Normal:
         return self * other
 
-    def __truediv__(self, other: Union["Normal", Any]) -> "Normal":
+    def __truediv__(self, other: C) -> Normal:
         if isinstance(other, (int, float, Tensor)):
             return self * (1 / other)
-
         if isinstance(other, NormalBase):
             raise NotImplementedError("Only support division by a constant for now")
         return NotImplemented
